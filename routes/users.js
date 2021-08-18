0476302ea71f13d5103c41f117b51ac436b665c2
Luckson Makanta <makantaluckson71@gmail.com>
2021-08-18 10:43:39 -0700

    Added the title

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => {res.render('login')});
-router.get('/register', (req, res) => {res.render('register')});
+router.get('/login', (req, res) => {res.render('login', {title : '| Sign in'})});
+router.get('/register', (req, res) => {res.render('register', {title : '| Register'})});
 
 module.exports = router;
